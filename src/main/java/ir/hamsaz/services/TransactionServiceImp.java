e9fc551aa70096899b411b2c62bc6b2474ca3598
Ali <ali.aberi1995@gmail.com>
2023-04-05 07:19:47 +0430

    change performance of Service

--- src/main/java/ir/hamsaz/services/TransactionServiceImp.java
+++ src/main/java/ir/hamsaz/services/TransactionServiceImp.java
@@ -2,15 +2,8 @@ package ir.hamsaz.services;
 
 import ir.hamsaz.models.Transactions;
 
-public class TransactionServiceImp implements TransactionService{
+public class TransactionServiceImp implements TransactionService {
 
-    public Transactions getP_conflict() {
-        return p_conflict;
-    }
-
-    public Transactions getB_conflict() {
-        return b_conflict;
-    }
 
     private Transactions p_conflict;
     private Transactions b_conflict;
@@ -20,39 +13,73 @@ public class TransactionServiceImp implements TransactionService{
     private int skh_count = 0;
 
     //when find a conflict that flag will be 1
-    private int flag = 0;
 
-    public TransactionServiceImp(){
+
+    public TransactionServiceImp() {
         p_conflict = new Transactions();
         b_conflict = new Transactions();
     }
 
     @Override
-    public void find_conflict(Transactions piramid,Transactions behpardakht){
+    public void find_conflict(Transactions piramid, Transactions behpardakht) {
 
         int bp_count = 0;
+        boolean Pflag;
+        int bp_rest;
+        Transactions b_read = new Transactions();
 
+        //move piramid list
         for (int i = 0; i < piramid.get_trs_size(); i++) {
-            flag = 0;
-            for (int j = bp_count; j < 10; j++) {
-                if(piramid.get_price(i).equals( behpardakht.get_price(j)) ) {
-                    bp_count += j;
-                    flag = 1;
+
+            //cheack piramid eqals behpardakht
+            Pflag = false;
+
+            //when arrive to end of behpardakht list, most cheack overflow
+            if(bp_count+10 > behpardakht.get_trs_size()){
+                bp_rest = behpardakht.get_trs_size() - bp_count;
+            }else{
+                bp_rest = 10;
+            }
+
+            //move behpardakht list
+            for (int j = bp_count; j < bp_rest; j++) {
+                if (piramid.get_price(0).equals(behpardakht.get_price(j))) {
+                    Pflag = true;
+                    piramid.RemoveTransaction(0);
+                    behpardakht.RemoveTransaction(j);
                     break;
-                }else{
-                    b_conflict.addTrs(behpardakht.get_price(j),behpardakht.get_traking_number(j));
+                }
+                else {
+                    b_read.addTrs(behpardakht.get_price(j), behpardakht.get_traking_number(j));
                 }
             }
-            if(flag == 0){
-                p_conflict.addTrs(piramid.get_price(i),piramid.get_traking_number(i));
+            if(Pflag){
+                if(b_read.isEmpty()){
+
+                }else{
+                    for (int k = 0; k < b_read.get_trs_size(); k++) {
+                        behpardakht.RemoveTransaction(0);
+                    }
+                    b_conflict.addTransactionsToTrs(b_read);
+                    b_read.RemoceAllTransactions();
+
+                }
+            }else{
+                b_read.RemoceAllTransactions();
+                p_conflict.addTrs(piramid.getTrs().get(0));
+                piramid.RemoveTransaction(0);
             }
+
         }
 
     }
 
+    public Transactions getP_conflict() {
+        return p_conflict;
+    }
 
-
-
-
+    public Transactions getB_conflict() {
+        return b_conflict;
+    }
 
 }
